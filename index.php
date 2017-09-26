06a7f92f1a4689ea95e2c51c435bb5ab44549630
pc26414f <32255837+pc26414f@users.noreply.github.com>
2017-09-26 10:04:01 +0800

    EC2 Instance IP V2

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
-<?php
-echo 'Current IP: '.$_SERVER['SERVER_ADDR'].'<br>';
-?>
+<html>
+  <head>
+    <title>Home</title>
+  </head>
+  <body>
+    <p>Current IP: <?php echo $_SERVER['SERVER_ADDR'];?></p>
+  </body>
+</html>

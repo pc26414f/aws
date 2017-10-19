f778cce9f62458193d5c6175f1c4dac021bab893
Gunther <32255837+pc26414f@users.noreply.github.com>
2017-10-19 18:14:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,6 @@ $dt = date('Y-m-d H:i').' '.date('T');
   <body>
     <p>Auto Deploy From GitHub Success!</p>
     <div>Current System Time: <?php echo $dt;?></div>
-    <div>By AWS CodeDeploy & GitHub Service</div>
+    <div>By AWS CodeDeploy with GitHub Integrations & Services</div>
   </body>
 </html>

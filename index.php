330c6fc44fcd1594bc53062b4d25f90ef4718d69
Gunther <32255837+pc26414f@users.noreply.github.com>
2017-10-23 16:58:45 +0800

    Test DB connection

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
 date_default_timezone_set('Asia/Taipei');
 $dt = date('Y-m-d H:i').'('.date('e').')';
+
+$dsn = 'mysql:host=db0.cu46sumlir5i.ap-northeast-1.rds.amazonaws.com;port=3306;dbname=LA;charset=utf8';
+$account = 'root';
+$password = '12345678';
+
+try{
+	$dbh = new PDO($dsn, $account, $password);
+	$stmt = $dbh->prepare('SELECT * FROM target');
+	$stmt->execute(array());
+	$response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}catch(PDOException $e){
+	echo $e->getMessage();
+}
 ?>
 <html>
   <head>
@@ -11,5 +24,13 @@ $dt = date('Y-m-d H:i').'('.date('e').')';
     <div>Current System Time: <?php echo $dt;?></div>
     <div>By AWS CodeDeploy with GitHub Integrations & Services</div>
     <div>BTW, GitHub Auto Deployment API Sucks!</div>
+    
+    <div>
+      <ul>
+        <?php foreach($response as $row){ ?>
+        <li>ID: <?php echo $row['id'];?>, Name: <?php echo $row['name'];?></li>
+        <?php } ?>
+      </ul>
+    </div>
   </body>
 </html>
